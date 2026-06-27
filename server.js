74015447c7f25175ddc2f1dd5505eeea3ddf67bd
shivam <devranishivam1121@gmail.com>
2026-06-27 14:16:00 +0530

    written cicd

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { configDotenv } from "dotenv";
+
+const app= express();
+
+configDotenv()
+
+app.get('/',(req,res)=>{
+
+    res.send("<h1>Welcome to Express Server</h1> <h2>Server is running on port 3000</h2>")
+})
+
+
+const port=process.env.PORT
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
